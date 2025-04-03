8e6fa9f459c8ff73f0165ebc71564c800c8948c1
YuranIgnatenko <yuran.ignatenko@yandex.ru>
2025-04-03 11:08:14 +0300

    append support agromachines

--- manager_calc.py
+++ manager_calc.py
@@ -1,44 +1,20 @@
 from constants import *
-
-class StorageResultCalculateN():
-	def __init__(self,NKR,NKR_round,NTR,NTR_round,NTO_3,NTO_3_round,NTO_2,NTO_2_round,NTO_1,NTO_1_round,N_CTO):
-		self.NKR = NKR
-		self.NKR_round = NKR_round
-		self.NTR = NTR
-		self.NTR_round = NTR_round
-		self.NTO_3 = NTO_3
-		self.NTO_3_round = NTO_3_round
-		self.NTO_2 = NTO_2
-		self.NTO_2_round = NTO_2_round
-		self.NTO_1 = NTO_1
-		self.NTO_1_round = NTO_1_round
-		self.N_CTO = N_CTO
-
-class StorageResultStringN():
-	def __init__(self,NKR_str,NTR_str,NTO_3_str,NTO_2_str,NTO_1_str,N_CTO_str):
-		self.NKR_str = NKR_str
-		self.NTR_str = NTR_str
-		self.NTO_3_str = NTO_3_str
-		self.NTO_2_str = NTO_2_str
-		self.NTO_1_str = NTO_1_str
-		self.N_CTO_str = N_CTO_str
-	def to_str(self) -> str:
-		res = "\n".join([self.NKR_str,self.NTR_str,self.NTO_3_str,self.NTO_2_str,self.NTO_1_str,self.N_CTO_str])
-		return res
+from models import *
 
 class Calculator():
 	def __init__(self):
 		pass
+
 	def round_number(self, num:float) -> int:
 		num = int(num + (0.5 if num > 0 else -0.5))
 		return num
 
-class CalculatorDiesel(Calculator):
+class CalculatorDiesel(Calculator):	
 	def __init__(self, data:ModelDataDiesel, count:int):
 		self.data = data
 		self.count = count 
 	
-	def get_N(self) -> tuple[StorageResultCalculateN,StorageResultStringN]:
+	def get_N(self) -> tuple[DataDieselResultN,DataDieselStringN]:
 		NKR  = self.data.NKR_U * self.count / self.data.NKR_N
 		NKR_round = self.round_number (NKR )
 		NTR  = self.data.NKR_U * self.count / self.data.NTR_N - NKR_round
@@ -58,8 +34,34 @@ class CalculatorDiesel(Calculator):
 		NTO_1_str = f"Nто-1 = {self.data.NKR_U} * {self.count} / {self.data.NTO_1_N} - {NKR} - {NTR} - {NTO_3} - {NTO_2} = {NTO_1}\nПринимаю: {NTO_1_round}"
 		N_CTO_str = f"Nсто = {self.data.NCTO} * {self.count} = {N_CTO}"
 
-		storage_result_calc_n = StorageResultCalculateN(NKR,NKR_round,NTR,NTR_round,NTO_3,NTO_3_round,NTO_2,NTO_2_round,NTO_1,NTO_1_round,N_CTO)
-		storage_result_string_n = StorageResultStringN(NKR_str,NTR_str,NTO_3_str,NTO_2_str,NTO_1_str,N_CTO_str)
+		storage_result_calc_n = DataDieselResultN(NKR,NKR_round,NTR,NTR_round,NTO_3,NTO_3_round,NTO_2,NTO_2_round,NTO_1,NTO_1_round,N_CTO)
+		storage_result_string_n = DataDieselStringN(NKR_str,NTR_str,NTO_3_str,NTO_2_str,NTO_1_str,N_CTO_str)
 
 		return (storage_result_calc_n, storage_result_string_n)
+	
+	# def get_T(self)-> tuple[DataResultT, DataStringT]:
+	# 	pass
+
+
+
+class CalculatorAgromachine(Calculator):
+	def __init__(self, data:ModelDataAgromachine, count:int):
+		self.data = data
+		self.count = count 
+	
+	def get_N(self) -> tuple[DataAgromachineResultN,DataAgromachineResultN]:
+		NTR = self.count * self.data.NTR
+		NTR_round = self.round_number(NTR)
+		NCTO = self.count * self.data.NCTO
+
+		NTR_str = f"Nтр = {self.count} * {self.data.NTR} = {NTR}\nПринимаю: {NTR_round}"
+		NCTO_str = f"Nсто = {self.count} * {self.data.NCTO} = {NCTO}"
+
+		storage_result_calc_n = DataAgromachineResultN(NTR, NTR_round, NCTO)
+		storage_result_string_n = DataAgromachineStringN(NTR_str, NCTO_str)
+
+		return (storage_result_calc_n, storage_result_string_n)
+	
+	# def get_T(self)-> tuple[StorageResultCalculateT,StorageResultStringT]:
+		# pass
 	
--- models.py
+++ models.py
@@ -15,7 +15,6 @@ class ModelDataDiesel():
 		self.TTO_1 = TTO_1
 		self.TCTO = TCTO
 
-
 class ModelDataAgromachine():
 	def __init__(self, NAME, NTR, NCTO, TTR, TCTO):
 		self.NAME = NAME
@@ -54,4 +53,43 @@ class ModelDataCar():
 		self.TTR_U1 = TTR_U1
 		self.TTR_D = TTR_D
 		self.TTR_Z = TTR_Z
+
+class DataDieselResultN():
+	def __init__(self,NKR,NKR_round,NTR,NTR_round,NTO_3,NTO_3_round,NTO_2,NTO_2_round,NTO_1,NTO_1_round,N_CTO):
+		self.NKR = NKR
+		self.NKR_round = NKR_round
+		self.NTR = NTR
+		self.NTR_round = NTR_round
+		self.NTO_3 = NTO_3
+		self.NTO_3_round = NTO_3_round
+		self.NTO_2 = NTO_2
+		self.NTO_2_round = NTO_2_round
+		self.NTO_1 = NTO_1
+		self.NTO_1_round = NTO_1_round
+		self.N_CTO = N_CTO
+
+class DataDieselStringN():
+	def __init__(self,NKR_str,NTR_str,NTO_3_str,NTO_2_str,NTO_1_str,N_CTO_str):
+		self.NKR_str = NKR_str
+		self.NTR_str = NTR_str
+		self.NTO_3_str = NTO_3_str
+		self.NTO_2_str = NTO_2_str
+		self.NTO_1_str = NTO_1_str
+		self.N_CTO_str = N_CTO_str
+	def to_str(self) -> str:
+		res = "\n".join([self.NKR_str,self.NTR_str,self.NTO_3_str,self.NTO_2_str,self.NTO_1_str,self.N_CTO_str])
+		return res
 	
+class DataAgromachineResultN():
+	def __init__(self, NTR, NTR_d, NCTO):
+		self.NTR = NTR
+		self.NTR_d = NTR_d
+		self.NCTO = NCTO
+		
+class DataAgromachineStringN():
+	def __init__(self, NTR_str, NCTO_str):
+		self.NTR_str = NTR_str
+		self.NCTO_str = NCTO_str
+	def to_str(self) -> str:
+		res = "\n".join([self.NTR_str, self.NCTO_str])
+		return res
